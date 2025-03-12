bdbfa8de161d3af6953a4246b4426c5354a2d69f
Sivarajan <104013594+Siva-Rajan-R@users.noreply.github.com>
2025-03-12 15:25:49 +0530

    Update registeration.py

--- api/routes/registeration.py
+++ api/routes/registeration.py
@@ -1,6 +1,6 @@
-import sys
+"""import sys
 import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))"""
 
 from fastapi import APIRouter,HTTPException,Depends
 from webauthn import generate_registration_options,verify_registration_response,options_to_json
