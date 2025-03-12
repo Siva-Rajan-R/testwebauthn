a99693eca31de09ba3692c8c0ddab9e6a2c45a08
Siva-Rajan-R <siva967763@gmail.com>
2025-03-12 16:02:14 +0530

    jj

--- api/routes/authentication.py
+++ api/routes/authentication.py
@@ -1,6 +1,6 @@
-"""import sys
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
-"""
 import os
 from fastapi import APIRouter,HTTPException,Depends,BackgroundTasks,Request
 from fastapi.responses import JSONResponse
