6026f924839eb0831eb967cbb18bbc4054326a37
Siva-Rajan-R <siva967763@gmail.com>
2025-03-12 15:47:39 +0530

    ,

--- database/main.py
+++ database/main.py
@@ -1,8 +1,8 @@
 from sqlalchemy.orm import declarative_base,sessionmaker
 from sqlalchemy import create_engine
+import os
 
-
-Engine=create_engine("postgresql://webauthdatabase_owner:npg_nRtUs85qDNYL@ep-empty-brook-a1ua68v1-pooler.ap-southeast-1.aws.neon.tech/webauthdatabase")
+Engine=create_engine(os.getenv("DATABASE_URL"))
 Base=declarative_base()
 
 def get_db_session():
